c529efcc11aeed666614f8cdfec3551a86b90ae6
tcoolican <tcoolican@usf.edu>
2024-11-10 18:39:50 -0500

    Create asset module with asset list and getAssetById function.

--- asset.js
+++ asset.js
@@ -1,1 +1,26 @@
+//Coding Project #15
+//Task 1: Create the Asset Module
+export const assetArray = [
+    { 
+        id: 1,
+        name: "Riddell",
+        type: "stock",
+        price: 250,
+        quantity: 400
+    },
 
+    {
+        id: 2,
+        name: "Schutt",
+        type: "bond",
+        price: 150,
+        quantity: 250
+    }]
+
+export function getAssetById(assetId) {
+    const assetWanted = assetArray.find(asset => assetId === asset.id)
+    if (assetWanted) {
+        return assetWanted
+    } else {
+        return null
+    }      }
