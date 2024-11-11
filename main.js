fc3265570bc693e50323285362f9d35948b66342
tcoolican <tcoolican@usf.edu>
2024-11-10 20:19:25 -0500

    edit

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
 //Coding Project #15
 //Task 4: Create the Main Application
-import { calculatePortfolioValue, getPortfolioAllocation } from "./portfolio.js";
+import { calculatePortfolioValue, getPortfolioAllocation} from "./portfolio.js";
 import { Transaction } from "./transaction.js";
 
-let totalPortfolioValue = calculatePortfolioValue();
-let portfolioDistribution = getPortfolioAllocation();
+//Declaring arguments
+let portfolioValue = calculatePortfolioValue()
+let portfolioAllocation = getPortfolioAllocation()
 
-const transaction1 = new Transaction(1, 'buy', 70);
-transaction1.updateQuantity();
+const transaction1 = new Transaction(1,'buy',35)
+transaction1.updateAssetQuantity()
+const transaction2 = new Transaction(2,'buy',25)
+transaction2.updateAssetQuantity()
+const transaction3 = new Transaction(1,'sell',95)
+transaction3.updateAssetQuantity()
+const transaction4 = new Transaction(2,'sell',65)
+transaction4.updateAssetQuantity()
 
-const transaction2 = new Transaction(2, 'buy', 60);
-transaction2.updateQuantity();
-
-const transaction3 = new Transaction(1, 'sell', 200);
-transaction3.updateQuantity();
-
-const transaction4 = new Transaction(2, 'sell', 150);
-transaction4.updateQuantity();
-
-console.log(`Portfolio Total Value: $${totalPortfolioValue}`);
-console.log(`Portfolio Distribution: ${portfolioDistribution}`);
+console.log(`Total Portfolio Value: ${portfolioValue}`)
+console.log(`Portfolio Allocation: ${portfolioAllocation}`)
 
 //HTML addition
 let div1 = document.getElementById("portfolio-value")
